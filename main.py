d9e647097ac0c1bc329e07e017abf9f2932142bf
ILDUS <ildus6696@gmail.com>
2024-08-31 12:10:25 +0300

    Add metod str -> float

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-import requests
 import json
 from pymongo import MongoClient
-from pprint import pprint
-from pymongo.errors import *
 
 client = MongoClient('localhost', 27017)
 db = client['books']
 books = db.books
-books.delete_many({})
 
-with open('books.json', 'r') as f:
-    data = json.load(f)
+# books.delete_many({})
+# with open('books.json', 'r') as f:
+#     data = json.load(f)
+#
+# _id = 0
+# for book in data:
+#     _id += 1
+#     book['_id'] = _id
+#     try:
+#         books.insert_one(book)
+#     except:
+#         print(book)
 
-_id = 0
-for book in data:
-    _id += 1
-    book['_id'] = _id
+"""Изменения стоимости с str на float"""
+count = 0
+bad_count = 0
+
+for book in books.find({}):
     try:
-        books.insert_one(book)
+        old_price = book["price"]
+        new_price = {'price': float(book["price"])}
+        books.update_one({'price': old_price}, {'$set': new_price})
+        count += 1
     except:
-        print(book)
+        bad_count += 1
+
+print(f'Количество измененных документов {count}')
+print(f'Количество неизмененных документов {bad_count}')
